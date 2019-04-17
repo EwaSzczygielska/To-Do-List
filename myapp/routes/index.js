155241dab71fcac137d12cbec97e7123be64a915
Adam <pelswick100@wp.pl>
2019-04-17 20:24:33 +0200

    server is working

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -7,4 +7,20 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+router.get('/style.css', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/style.css'));
+});
+
+router.get('/src/img/logo.png', function (req, res, next) {
+  res.sendFile(path.join(__dirname + '/src/img/logo.png'));
+});
+
+router.get('/src/img/main-bg.svg', function (req, res, next) {
+  res.sendFile(path.join(__dirname + '/src/img/main-bg.svg'));
+});
+
+router.get('/src/js/index.js', function (req, res, next) {
+  res.sendFile(path.join(__dirname + '/src/js/index.js'));
+});
+
 module.exports = router;
