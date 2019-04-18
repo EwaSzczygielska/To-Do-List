3eaecdfd9dfc8bb079eec1401513449bda28e91c
Ewa <ewa.szczygielska@onet.pl>
2019-04-18 17:47:03 +0200

    update to routes

--- myapp/routes/src/js/new-task.js
+++ myapp/routes/src/js/new-task.js
@@ -1,16 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
+const addBtn = document.querySelector(".add-button");
+const toDoList = document.querySelector(".to-do-list");
 
-var addbtn = document.getElementById("add-button");
+addBtn.addEventListener("click", e => {
+    e.preventDefault();
+    const currentDate = new Date();
+    const idCounter = toDoList.childElementCount;
+    let newTask = {
+        id: idCounter,
+        message: " " + idCounter + ". " + document.forms[0].elements[0].value + " ",
+        priority: document.forms[0].elements[4].value,
+        category: "",
+        deadline: document.forms[0].elements[5].value,
+        createDate: currentDate.getDate() + "." + 
+                    (currentDate.getMonth()+1) + "." +
+                    currentDate.getFullYear()
+    };
+    if (document.forms[0].elements[1].checked)  newTask.category = "home";
+    if (document.forms[0].elements[2].checked)  newTask.category = "work"; 
+    if (document.forms[0].elements[3].checked)  newTask.category = "social";
+    
+    const newTaskElement = document.createElement("div");
+    newTaskElement.classList.add("task");
+    const priorityIndicator = document.createElement("i");
+    priorityIndicator.classList.add("priority-icon", "fas", "fa-dot-circle");
 
-function addNewTask() {
-    var task = document.getElementById("maininput").value;
-    var taskContainer = document.querySelector(".to-do-list");
-    var newTask = document.createElement('div');
-    console.log(task);
-    var newDiv = newTask.classList.add('task');
-    //taskContainer.appendChild(newDiv);
-}
+    if (newTask.priority == "1") priorityIndicator.classList.add("priority-low");
+    else if (newTask.priority == "2") priorityIndicator.classList.add("priority-mid");
+    else if (newTask.priority == "3") priorityIndicator.classList.add("priority-high");
 
-addbtn.addEventListener('click', addNewTask());
+    const actionIcons = document.createElement("span");
+    const checkIcon = document.createElement("i");
+    const editIcon = document.createElement("i");
+    const deleteIcon = document.createElement("i");
+    checkIcon.classList.add("action-icon", "far", "fa-check-circle");
+    editIcon.classList.add("action-icon", "far", "fa-edit");
+    deleteIcon.classList.add("action-icon", "fas", "fa-trash-alt");
+    actionIcons.appendChild(checkIcon);
+    actionIcons.appendChild(editIcon);
+    actionIcons.appendChild(deleteIcon);
 
+    const taskContent = document.createElement("p");
+    taskContent.appendChild(document.createTextNode(newTask.message));
+    taskContent.appendChild(actionIcons);
+
+    if (newTask.category == "home") taskContent.classList.add("ctg-home");
+    else if (newTask.category == "work") taskContent.classList.add("ctg-work");
+    else if (newTask.category == "social") taskContent.classList.add("ctg-social");
+
+    newTaskElement.appendChild(priorityIndicator);
+    newTaskElement.appendChild(taskContent);
+    toDoList.appendChild(newTaskElement);
+
+    document.querySelector("#add-task-form").reset();
 });
