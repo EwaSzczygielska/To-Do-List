8b2fd20d7a4d06cfb71c62c59161a5f79834c9f2
Kacper <kacperszymula@wp.pl>
2019-04-25 11:30:11 +0200

    try v2

--- myapp/routes/src/js/database.js
+++ myapp/routes/src/js/database.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/to_do_list')
-.then(() => console.log('Connected to MongoDb'))
-.catch(err => console.error('Error', err))
+//mongoose.connect('mongodb://localhost/to_do_list')
+//.then(() => console.log('Connected to MongoDb'))
+//.catch(err => console.error('Error', err))
