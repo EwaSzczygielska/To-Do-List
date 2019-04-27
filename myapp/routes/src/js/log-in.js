ea58cc025d3f8b83056d1556e03a490b9f2079e6
Kinga <gniedziejko.kinga@gmail.com>
2019-04-27 14:09:18 +0200

    Update log-in.js

--- myapp/routes/src/js/log-in.js
+++ myapp/routes/src/js/log-in.js
@@ -3,10 +3,17 @@ var logInBtn = document.querySelector('.log-in-button');
 var SignUpBtn = document.querySelector('.sign-up-button');
 
 logInBtn.addEventListener('click', function (){
+    var username = document.querySelector('.username-log').value;
+    var password = document.querySelector('.password-log').value;
+
+    //if username and password correct
     logInBox.style.display = 'none';
 })
 
 SignUpBtn.addEventListener('click', function () {
+    var username = document.querySelector('.username-sign').value;
+    var password = document.querySelector('.password-sign').value;
+
     logInBox.style.display = 'none';
 })
 
